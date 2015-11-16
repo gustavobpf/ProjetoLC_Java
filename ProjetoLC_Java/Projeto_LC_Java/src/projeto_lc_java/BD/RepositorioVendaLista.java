891c6cc9e4ac011a3a4e1cdd4d901999992e8ddd
gustavobpf <gustavo.alfaeng@gmail.com>
2015-11-16 11:18:17 -0200

    Atualizacao_repositorio_2015-11-16_02

---
+++ ProjetoLC_Java/Projeto_LC_Java/src/projeto_lc_java/BD/RepositorioVendaLista.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package projeto_lc_java.BD;
+import projeto_lc_java.Venda;
+/**
+ *
+ * @author NATI4
+ */
+public class RepositorioVendaLista {
+    private Venda venda;
+    private RepositorioVendaLista ponteiro;
+    
+    public RepositorioVendaLista(Venda venda){
+        this.venda = venda;
+        this.ponteiro = null;
+    }
+    
+    public Venda getVenda(){
+        return this.venda;
+    }
+    public void setVenda(Venda venda){
+        this.venda = venda;
+    }
+    
+    public RepositorioVendaLista getPonteiro(){
+        return this.ponteiro;
+    }
+    
+    public void setPonteiro(RepositorioVendaLista proximo){
+        this.ponteiro = proximo;
+    }
+          
+    public void inserirCompra(RepositorioVendaLista proximo){
+        RepositorioVendaLista aux;
+        if(this.ponteiro.equals(null)){
+            this.ponteiro.setPonteiro(proximo);
+        }else{
+            aux = this.ponteiro;
+            inserirCompra(aux);
+        }
+    }
+    
+    public Venda consultarVenda(String nf){
+        RepositorioVendaLista aux;
+        if(this.venda.getNf().equals(nf)){
+            return this.venda;
+        }else{
+            aux = this.ponteiro;
+            consultarVenda(aux.venda.getNf());
+        }
+        return null;
+    }
+    
+    public void excluirVenda(RepositorioVendaLista proximo){
+        
+        if(this.ponteiro.getVenda().getNf().equals(proximo.getVenda().getNf())){
+            this.ponteiro.setPonteiro(proximo.getPonteiro());
+            proximo.setPonteiro(null);
+        }else{
+            excluirVenda(this.ponteiro.getPonteiro());
+        }
+    }
+    
+    public void atualizarVenda(String nf, Venda venda){
+        if(this.ponteiro.getVenda().getNf().equals(nf)){
+            this.ponteiro.setVenda(venda);
+        }else{
+            atualizarVenda(nf,this.ponteiro.getPonteiro().getVenda());
+        }
+    }
+}
